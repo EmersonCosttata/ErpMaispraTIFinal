959522fd1c528de46cc8492bdbd8b5bf02b65492
Emerson <euemerson1@gmail.com>
2025-01-14 11:12:01 -0300

    Final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/': {
+        target: 'http://52.205.89.4:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
