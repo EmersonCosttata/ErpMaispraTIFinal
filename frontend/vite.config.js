e29688d681ed99693a8956eeab8201bed5cce668
Emerson <138705956+EmersonCosttata@users.noreply.github.com>
2025-01-14 11:18:51 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/': {
-        target: 'http://44.223.5.130:8080',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+
 })
